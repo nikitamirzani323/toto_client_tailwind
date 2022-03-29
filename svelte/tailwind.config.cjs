8d41d548e849de25d14fa406c674c6b9468ebcaf
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2022-03-29 23:33:25 +0700

    Update Navbar + tailwind Config

--- svelte/tailwind.config.cjs
+++ svelte/tailwind.config.cjs
@@ -5,6 +5,20 @@ module.exports = {
   },
   plugins: [require('tailwind-scrollbar'),require("tailwind-scrollbar-hide"),require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+    themes: [
+      {
+        myLight: {
+          "primary": "#91f7bc",      
+          "secondary": "#94e07d",
+          "accent": "#e50ba0",
+          "neutral": "#1A1C28",
+          "base-100": "#E7E3E8",
+          "info": "#70CAE6",
+          "success": "#209D82",
+          "warning": "#FCA131",
+          "error": "#EB4228",
+        },
+      },
+      "light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
   },
 }
