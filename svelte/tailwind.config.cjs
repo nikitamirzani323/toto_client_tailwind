392a058913c5b6281db303273d8323e2c9189a0a
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2022-05-02 17:26:02 +0700

    Update Tailwind Configure

--- svelte/tailwind.config.cjs
+++ svelte/tailwind.config.cjs
@@ -20,6 +20,6 @@ module.exports = {
           "error": "#EB4228",
         },
       },
-      "light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+      "emerald", "dracula"],
   },
 }
